9a4f9b6b1eee3974fd392e5703c533f0898cdcc0
raushan kumar <iamrk291@gmail.com>
2020-08-18 02:53:28 +0530

    Added new files for images and video input

--- darknet.py
+++ darknet.py
@@ -306,12 +306,28 @@ class Darknet(nn.Module):
                 conv.weight.data.copy_(conv_weights)
 
 
+def get_test_input():
+    img = cv2.imread("dog-cycle-car.png")
+    img = cv2.resize(img, (608,608))          #Resize to the input dimension
+    img_ =  img[:,:,::-1].transpose((2,0,1))  # BGR -> RGB | H X W C -> C X H X W 
+    img_ = img_[np.newaxis,:,:,:]/255.0       #Add a channel at 0 (for batch) | Normalise
+    img_ = torch.from_numpy(img_).float()     #Convert to float
+    img_ = Variable(img_)                     # Convert to Variable
+    return img_
 
-net = Darknet(r"C:\Users\kiit\Desktop\Deep_Learning_git\Deep-Learning\cfg\yolov3.cfg")
-print(net)
-net.load_weights(r"C:\Users\kiit\Desktop\Deep_Learning_git\Deep-Learning\weights\yolov3.weights")
-print('model weight loaded')
-input = torch.randn(5,3,608,608)
-output = net(input, CUDA=False)
-print(output)
-print(output.size())
+model = Darknet("cfg/yolov3.cfg")
+model.load_weights("weights/yolov3.weights")
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print (pred)
+print (pred.size())
+
+
+# net = Darknet(r"C:\Users\kiit\Desktop\Deep_Learning_git\Deep-Learning\cfg\yolov3.cfg")
+# print(net)
+# net.load_weights(r"C:\Users\kiit\Desktop\Deep_Learning_git\Deep-Learning\weights\yolov3.weights")
+# print('model weight loaded')
+# input = torch.randn(5,3,608,608)
+# output = net(input, CUDA=False)
+# print(output)
+# print(output.size())
